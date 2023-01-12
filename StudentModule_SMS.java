e474d241a633da37607936cfebda67aeb62d24b6
Quinuel Tabot  N-A <97244109+QUIN04@users.noreply.github.com>
2023-01-12 11:21:29 +0100

    Add files via upload

---
+++ StudentModule_SMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.module_sms;
+
+/**
+ *
+ * @author Ashu-Mbi
+ */
+public class StudentModule_SMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
